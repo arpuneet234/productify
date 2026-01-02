b8e937da463d70f84d64c6167016128cd7649b6e
Puneet Arora <arorapuneet234@gmail.com>
2026-01-02 19:39:29 +0530

    Search Functionality Done

--- components/Body.js
+++ components/Body.js
@@ -1,12 +1,43 @@
 import ProductCard from "./ProductCard";
 import { productsList } from "../constants";
+import { useState } from "react";
+
+function filter(searchText, productsList) {
+  let filteredListd = productsList.filter((product) => {
+    return product.title.includes(searchText);
+  });
+  return filteredListd;
+}
 
 const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [filteredList, setfilteredList] = useState(productsList);
+
   return (
-    <div className="product-list">
-      {productsList.products.map((product, index) => {
-        return <ProductCard {...product} key={index} />;
-      })}
+    <div>
+      <div className="Search-Bar">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          onClick={(e) => {
+            setfilteredList(filter(searchText, productsList));
+          }}
+        >
+          Search
+        </button>
+      </div>
+
+      <div className="product-list">
+        {filteredList.map((product, index) => {
+          return <ProductCard {...product} key={index} />;
+        })}
+      </div>
     </div>
   );
 };
--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,11 +1,9 @@
 import { productsList } from "../constants";
 
-const ProductCard = ({ title, thumbnail, tags, price, rating }) => {
-  console.log(productsList?.products[20]?.id);
-  console.log(thumbnail);
+const ProductCard = ({ title, image, tags, price, rating }) => {
   return (
     <div className="card">
-      <img src={thumbnail}></img>
+      <img src={image}></img>
       <h1>{title}</h1>
       <h2>{tags?.join(", ")}</h2>
       <h3>Price : {price} $</h3>
